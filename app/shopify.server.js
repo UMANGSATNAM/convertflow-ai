089b65e0b61ad602ef772974f1f539a4a4e88d2c
Umangpatel11 <umangptl11@gmail.com>
2026-02-21 11:48:57 +0530

    fix: disable unstable_newEmbeddedAuthStrategy to fix login redirect loop

--- app/shopify.server.js
+++ app/shopify.server.js
@@ -20,7 +20,7 @@ const shopify = shopifyApp({
   sessionStorage,
   distribution: AppDistribution.AppStore,
   future: {
-    unstable_newEmbeddedAuthStrategy: true,
+    unstable_newEmbeddedAuthStrategy: false,
   },
   ...(process.env.SHOP_CUSTOM_DOMAIN
     ? { customShopDomains: [process.env.SHOP_CUSTOM_DOMAIN] }
